3432d84eacd634d4617dc320bd7a804f4fa74f91
wmui <1719442545@qq.com>
2017-08-03 10:12:43 +0800

    create

--- server/md5.js
+++
@@ -1,7 +0,0 @@
-let crypto = require('crypto');
-// md5 加密
-module.exports = function (content) {
-    let md5 = crypto.createHash('md5');
-    let newContent = md5.update(content).digest('base64');
-    return newContent;
-};

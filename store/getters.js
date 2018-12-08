1444178b372bc78f7ee9a3c398b5f2098a43ddd2
wmui <qq22337383@gmail.com>
2018-12-08 11:20:38 +0800

    fix: baseUrl

--- store/getters.js
+++ store/getters.js
@@ -4,8 +4,7 @@ export default {
     if (process.env.NODE_ENV === 'production') {
       host = 'https://api.86886.wang/v1'
     } else {
-      // host = 'http://127.0.0.1:3010/v1'
-      host = 'https://api.86886.wang/v1' // 仅能查看结果
+      host = 'http://127.0.0.1:3010/v1'
     }
     return host
   },

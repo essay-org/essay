e934eec2c43706fcad4e0239e7af4caa3f41133a
wmui <qq22337383@gmail.com>
2018-12-08 23:46:59 +0800

    fix: ignore

---
+++ server/config/production.js
@@ -0,0 +1,30 @@
+module.exports =  {
+  user: {
+    role: 'superAdmin',
+    username: 'q',
+    password: 'q',
+    nickname: 'Essay',
+    email: 'qq22337383@gmail.com',
+    motto: 'Never too old to learn',
+  },
+  jwt: {
+    secret: 'essay',
+    expiresIn: 1296000,
+  },
+  mongodb: {
+    host: '127.0.0.1',
+    database: 'essay',
+    port: 27017,
+    username: '',
+    password: '',
+  },
+  githubConfig: {
+    githubClient: '',
+    githubSecret: '',
+    scope: 'user',
+  },
+  emailConfig: {
+    user: '',
+    pass: '',
+  },
+}

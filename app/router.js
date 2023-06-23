96cd8986b3b4e6fc9ca8bb5a04f66b06f8c16cde
wmui <waynewang@primecare.group>
2023-06-23 14:27:12 +0800

    feat: auth

--- app/router.js
+++ app/router.js
@@ -19,11 +19,11 @@ module.exports = app => {
   // 获取文章
   router.get('/api/post', controller.post.find);
   // 添加文章
-  router.post('/api/post', controller.post.save);
+  router.post('/api/post', auth(), controller.post.save);
   // 修改文章
   // router.put('/api/post', controller.post.save);
   // 删除文章
-  router.delete('/api/post/:id', controller.post.remove);
+  router.delete('/api/post/:id', auth(), controller.post.remove);
   // 系统安装
   router.get('/install', controller.user.install);
   // 登录模板
@@ -31,21 +31,21 @@ module.exports = app => {
   // 登录验证码
   router.get('/api/captcha', controller.user.captcha);
   // 获取管理员信息
-  router.get('/api/user', controller.user.find);
+  router.get('/api/user', auth(), controller.user.find);
   // 修改管理员信息
-  router.put('/api/user', controller.user.save);
+  router.put('/api/user', auth(), controller.user.save);
   // 管理员登录
   router.post('/api/login', controller.user.login);
   // 初始化管理员
   router.post('/api/user', controller.user.init);
   // 设置
-  router.get('/setting', controller.option.setting);
+  router.get('/setting', auth(true), controller.option.setting);
   // 修改或新增seo
-  router.post('/api/seo', controller.option.saveSeo);
-  router.get('/api/seo', controller.option.findSeo);
+  router.post('/api/seo', auth(), controller.option.saveSeo);
+  router.get('/api/seo', auth(), controller.option.findSeo);
 
   // 资源上传
-  router.post('/api/upload', controller.post.upload);
+  router.post('/api/upload', auth(), controller.post.upload);
   // version
   router.get('/api/version', controller.option.version);
   // 其他
